9f771d75ac445b8b375345056a2845ea33a5d280
albertogioia93 <albertogioia@hotmail.it>
2023-03-16 20:02:06 +0100

    prove: clicco su avatar lista sx e lo vedo in h.dx

--- milestone1/js/main.js
+++ milestone1/js/main.js
@@ -3,25 +3,53 @@
 // collegamento globale al div con id=app del file index.html
 const { createApp } = Vue
 
+// createApp({
+//   data() {
+//     return {
+//         contatti: [
+//             {
+//                 nome: 'Michele',
+//                 avatar: 'img/avatar_1.jpg'
+//             },
+//             {
+//                 nome: 'Fabio',
+//                 avatar: 'img/avatar_2.jpg'
+//             },
+//             {
+//                 nome: 'Samuele',
+//                 avatar: 'img/avatar_3.jpg'
+//             },
+//         ]
+
+//     }
+//   }
+// }).mount('#app')
+
+
 createApp({
-  data() {
-    return {
-        contatti: [
-            {
-                nome: 'Michele',
-                avatar: 'img/avatar_1.jpg'
-            },
-            {
-                nome: 'Fabio',
-                avatar: 'img/avatar_2.jpg'
-            },
-            {
-                nome: 'Samuele',
-                avatar: 'img/avatar_3.jpg'
-            },
-        ]
-      
+    data() {
+        return {
+            contatti: [
+                {
+                    nome: 'Michele',
+                    avatar: 'img/avatar_1.jpg'
+                },
+                {
+                    nome: 'Fabio',
+                    avatar: 'img/avatar_2.jpg'
+                },
+                {
+                    nome: 'Samuele',
+                    avatar: 'img/avatar_3.jpg'
+                },
+            ],
+            contattoSelezionato: null
+        }
+    },
+    methods: {
+        selezionaContatto(contatto) {
+            this.contattoSelezionato = contatto;
+        }
     }
-  }
 }).mount('#app')
 
