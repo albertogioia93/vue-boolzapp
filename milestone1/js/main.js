cee87a4f9e73ddb2b0a75137f9493f1f119fc013
albertogioia93 <albertogioia@hotmail.it>
2023-03-17 14:55:28 +0100

    click su main-sx.ul.li = mostrato in header-dx

--- milestone1/js/main.js
+++ milestone1/js/main.js
@@ -3,23 +3,77 @@
 // collegamento globale al div con id=app del file index.html
 const { createApp } = Vue
 
-
 createApp({
     data() {
         return {
             contatti: [
                 {
                     nome: 'Michele',
-                    avatar: 'img/avatar_1.jpg'
+                    avatar: 'img/avatar_1.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
                 },
                 {
                     nome: 'Fabio',
-                    avatar: 'img/avatar_2.jpg'
+                    avatar: 'img/avatar_2.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ]
                 },
                 {
                     nome: 'Samuele',
-                    avatar: 'img/avatar_3.jpg'
-                },
+                    avatar: 'img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ]
+                }
+            
             ],
             contattoSelezionato: null
         }
@@ -27,6 +81,7 @@ createApp({
     methods: {
         selezionaContatto(contatto) {
             this.contattoSelezionato = contatto;
+            console.log('mi hai cliccato');
         }
     }
 }).mount('#app')
